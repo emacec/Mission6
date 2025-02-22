8049361ff911de1c35772e8c63ca8ddd738c8509
emacec <emacec@byu.edu>
2025-02-21 23:32:47 -0700

    mission07

--- Mission6/Controllers/HomeController.cs
+++ Mission6/Controllers/HomeController.cs
@@ -8,12 +8,12 @@ namespace Mission6.Controllers
     public class HomeController : Controller
     {
         private ApplicationContext _context;
-    
+
         public HomeController(ApplicationContext temp) //Constructor
         {
             _context = temp;
         }
-        
+
         public IActionResult Index()
         {
             return View();
@@ -33,7 +33,7 @@ namespace Mission6.Controllers
                 _context.Movies.Add(response); // add record to the database
                 _context.SaveChanges();
             }
-            
+
 
             return View("Confirmation", response);
 
@@ -43,5 +43,52 @@ namespace Mission6.Controllers
         {
             return View();
         }
+
+        public IActionResult List()
+        {
+            //Linq
+            var applications = _context.Movies
+                .Where(x => x.Edited == false)
+                .OrderBy(x => x.Title).ToList();
+            return View(applications);
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            var recordToEdit = _context.Movies
+                .Single(x => x.ApplicationID == id);
+
+            return View("FilmForm", recordToEdit);
+        }
+
+        [HttpPost]
+
+        public IActionResult Edit(Application updatedInfo)
+        {
+            _context.Update(updatedInfo);
+            _context.SaveChanges();
+
+            return RedirectToAction("List");
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int id)
+        {
+            var recordToDelete = _context.Movies
+                .Single(x => x.ApplicationID == id);
+            return View(recordToDelete);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Application delete)
+        {
+            _context.Movies.Remove(delete);
+            _context.SaveChanges();
+
+            return RedirectToAction("List");
+
+        }
+
     }
 }
--- Mission6/Models/Application.cs
+++ Mission6/Models/Application.cs
@@ -1,12 +1,15 @@
-﻿namespace Mission6.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mission6.Models
 {
     public class Application
     {
-       
+        [Required]
         public int ApplicationID { get; set; }
         public string Category { get; set; }
         public string Title { get; set; }
         public int Year { get; set; }
+        [Range(1888,2025)]
         public string Director { get; set; }
         public int Rating { get; set; }
         public string LentTo { get; set; }
